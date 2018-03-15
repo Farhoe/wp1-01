40dea08060b5722a1c671158b2ddf1b2d79ede89
Sp00dlepuss <matejdalekorej@gmail.com>
2018-03-15 12:11:29 +0100

    Omylem jsem si odstranil složku s projekty. Tak jsem to musel předělat.

---
+++ Pr2/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>2. práce</title>
+  </head>
+  <body>
+    <h1>Obsah Kruhu</h1>
+    <?php
+    define('PI',3.14);
+    $polomer="r*r";
+    $r=6;
+    $polomer="3.14*$r";
+     ?>
+
+     <?php
+
+     echo "S = PI . r² = S = " . PI . "*$polomer" ;
+      ?>
+
+  </body>
+</html>
